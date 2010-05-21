5203ea0347055bb5b3b96b7ddb6624e8ff7e99ed
amdayton <amdayton@gmail.com>
2010-05-21 14:28:24 -0500

    update to how config stuff is handled, added kabaret page

---
+++ pages/kabaret.php
@@ -0,0 +1,2 @@
+<h1>Kabaret: 48 hours of MNKINO!</h1>
+<p>We've locked in the date for our first MNKINO Kabaret! What's a Kabaret? It's a 48-hour filmmaking extravaganza, a weekend of MNKINO. It's all happening <strong>July 23rd–25th</strong> — we'll kick off with a Friday screening, after which we'll announce the Kabaret mystery meat. After that you'll have 48 hours to finish a film, screened the following Sunday.</p>
--- scripts/boot.php
+++ scripts/boot.php
@@ -2,7 +2,7 @@
 require_once('lib/spyc.php');
 require_once('lib/magpierss/rss_fetch.inc');
 
-// require_once('environment.php');
+require_once(dirname(__FILE__).'/../config/environment.php');
 
 # connect to database
 // $connection = mysql_connect($ENV['server'], $ENV['username'], $ENV['password']) or die('Could not connect'.mysql_error());
@@ -13,20 +13,24 @@ require_once('lib/magpierss/rss_fetch.inc');
 // $CONFIG = mysql_fetch_assoc($config_r);
 // mysql_free_result($config_r);
 
-$CONFIG = array(
-	// 'web_root' => '/Users/amdayton/Sites/mnkino.com/',
-	 'web_root' => '/users/home/amdayton/domains/mnkino.com/web/public/',
-	'mail_to' => 'mnkino@gmail.com',
-	'flickr_api_key' => '',
-	'admin_username' => '',
-	'admin_password' => '',
-	'blip_tv_username' => '',
-	'blip_tv_password' => '',
-	'blip_tv_url' => '',
-	'flickr_username' => '',
-	'flickr_password' => '',
-	'flickr_url' => ''
-);
+// $environment = Spyc::YAMLLoad(dirname(__FILE__).'/../config/environment.yml');
+
+// $CONFIG = $environment['config'];
+
+// $CONFIG = array(
+// 	// 'web_root' => '/Users/amdayton/Sites/mnkino.com/',
+// 	 'web_root' => '/users/home/amdayton/domains/mnkino.com/web/public/',
+// 	'mail_to' => 'mnkino@gmail.com',
+// 	'flickr_api_key' => '',
+// 	'admin_username' => '',
+// 	'admin_password' => '',
+// 	'blip_tv_username' => '',
+// 	'blip_tv_password' => '',
+// 	'blip_tv_url' => '',
+// 	'flickr_username' => '',
+// 	'flickr_password' => '',
+// 	'flickr_url' => ''
+// );
 
 include_once('page.php');
 // include_once('flickr.php');
