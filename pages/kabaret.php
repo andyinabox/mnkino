a2551d3673e49577594616633a11933327c11317
amdayton <amdayton@gmail.com>
2010-06-21 15:56:09 -0500

    added kabaret page

--- pages/kabaret.php
+++ pages/kabaret.php
@@ -1,2 +1,35 @@
-<h1>Kabaret: 48 hours of MNKINO!</h1>
-<p>We've locked in the date for our first MNKINO Kabaret! What's a Kabaret? It's a 48-hour filmmaking extravaganza, a weekend of MNKINO. It's all happening <strong>July 23rd–25th</strong> — we'll kick off with a Friday screening, after which we'll announce the Kabaret mystery meat. After that you'll have 48 hours to finish a film, screened the following Sunday.</p>
+<h1>MNKINO Kabaret!</h1>
+<p>Adhering to Kino standards we at MNKINO are takin' to tradition and throwing a Kabaret! (MNKINO-style)</p>
+
+<h2>What is a Kabaret?</h2>
+
+<p>It is truly the cornerstone in the international Kino mission. The Kabaret is the labs in which filmmakers, actors, musicians, and other film enthusiasts come together to create films.</p>
+<p>Participants have a 48 hour period to brainstorm, shoot, edit and present their video results to the public. Like KINO monthly screenings, a Kabaret is open to any and everyone, regardless of skill or supply! The idea is to collaborate skill, vision and technical resources with others to create one final product.</p>
+
+<h2>What is the MNKINO KABARET?</h2>
+
+<p>We will meet for a screening of selected submissions from the past year of MNKINO. At the end of the screening, the Mystery Meat of the Kabaret will be revealed and participants will be set free to start their videos! Groups will have 48 hours until the final screening of the Kabaret where we will screen their results!</p>
+
+<h3>The Lab</h3>
+<p>A Kabaret wouldn't be a Kabaret without a proper lab! The official MNKINO Lab will be located within the West Bank Social Center and will be open and monitored throughout the entirety of the 48 hours. Bring your equipment and set up shop directly following the initial screening!</p>
+
+<h3>No Group? No Problem.</h3>
+<p>Individuals that don't arrive with a set group will be matched up with a group after the screening.</p>
+
+<h3>No Experience? No Problem!</h3>
+<p>Kabaret's are a great opportunity to learn the art of making short films! Just show up to the screening and we will match you up with a film-savvy group.</p>
+
+<h2>What are the guidelines?</h2>
+
+<ol>
+	<li>Each group/individual must register each planned Kabaret film on the opening night of the MNKINO Kabaret. One person from each group will be designated as the primary director/contact.</li>
+	<li>Kabaret films must be made within the period of the Kabaret event. This includes all captured footage.</li>
+ 	<li>Films must incorporate the Mystery Meat for the Kabaret. This will be announced on the opening night and posted on our website.</li>
+	<li>Films must be <strong>no longer than 5 minutes long</strong>.</li>
+	<li>Completed film(s) must be submitted in digital file format (.mov, .avi, etc), Mini-DV format or on DVD. Double check that the file works before you submit!</li>
+	<li>All completed film(s) must be checked in by the designated primary director at the final screening. They msut be submitted by hand, 15 minutes before the beginning of the Kabaret screening.</li>
+	<li>Film must include the MNKINO KABARET logo and current year, in the end credits. (Our logo will be available for download).</li>
+	<li>Have fun and work as a team! Duh.</li>
+</ol>
+
+<p>Hope to see you at our FIRST EVER MNKINO KABARET! Remember, you can come with our without experience or technology. We will find you a team!</p>
