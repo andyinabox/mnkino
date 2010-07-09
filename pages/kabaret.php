83bc1651d0bfa11e4252df409cecc5ec158eb0dc
amdayton <amdayton@gmail.com>
2010-07-09 07:46:52 -0500

    Updates to kabaret page

--- pages/kabaret.php
+++ pages/kabaret.php
@@ -1,35 +1,38 @@
 <h1>MNKINO Kabaret!</h1>
-<p>Adhering to Kino standards we at MNKINO are takin' to tradition and throwing a Kabaret! (MNKINO-style)</p>
+<p>Adhering to Kino standards we at MNKINO are takin' to tradition and throwing a Kabaret! (MNKINO-style).</p>
 
 <h2>What is a Kabaret?</h2>
 
-<p>It is truly the cornerstone in the international Kino mission. The Kabaret is the labs in which filmmakers, actors, musicians, and other film enthusiasts come together to create films.</p>
-<p>Participants have a 48 hour period to brainstorm, shoot, edit and present their video results to the public. Like KINO monthly screenings, a Kabaret is open to any and everyone, regardless of skill or supply! The idea is to collaborate skill, vision and technical resources with others to create one final product.</p>
+<p>It is truly the cornerstone in the international Kino mission. The Kabaret is the lab in which filmmakers, actors, musicians, actors, and other film enthusiasts come together to create films.</p>
+<p>Participants have a 48-hour period to brainstorm, shoot, edit and present their video results to the public. Like KINO monthly screenings, a Kabaret is open to any and everyone, regardless of skill or supply! The idea is to collaborate skill, vision and technical resources with others to create one final product.</p>
 
-<h2>What is the MNKINO KABARET?</h2>
 
-<p>We will meet for a screening of selected submissions from the past year of MNKINO. At the end of the screening, the Mystery Meat of the Kabaret will be revealed and participants will be set free to start their videos! Groups will have 48 hours until the final screening of the Kabaret where we will screen their results!</p>
+<object width="500" height="375"> <param name="flashvars" value="offsite=true&lang=en-us&page_show_url=%2Fsearch%2Fshow%2F%3Fq%3Dkino%2Bkabaret%26ss%3D0%26ct%3D0%26mt%3Dphotos%26adv%3D1&page_show_back_url=%2Fsearch%2F%3Fq%3Dkino%2Bkabaret%26ss%3D0%26ct%3D0%26mt%3Dphotos%26adv%3D1&method=flickr.photos.search&api_params_str=&api_text=kino+kabaret&api_tag_mode=bool&api_media=photos&api_sort=relevance&jump_to=&start_index=0"></param> <param name="movie" value="http://www.flickr.com/apps/slideshow/show.swf?v=71649"></param> <param name="allowFullScreen" value="true"></param><embed type="application/x-shockwave-flash" src="http://www.flickr.com/apps/slideshow/show.swf?v=71649" allowFullScreen="true" flashvars="offsite=true&lang=en-us&page_show_url=%2Fsearch%2Fshow%2F%3Fq%3Dkino%2Bkabaret%26ss%3D0%26ct%3D0%26mt%3Dphotos%26adv%3D1&page_show_back_url=%2Fsearch%2F%3Fq%3Dkino%2Bkabaret%26ss%3D0%26ct%3D0%26mt%3Dphotos%26adv%3D1&method=flickr.photos.search&api_params_str=&api_text=kino+kabaret&api_tag_mode=bool&api_media=photos&api_sort=relevance&jump_to=&start_index=0" width="500" height="375"></embed></object>
+
+<h2>What is the MNKINO Kabaret?</h2>
+
+<p>We will meet for an open screening, inviting participants to submit anything under five minutes. At the end of the screening, the Mystery Meat (theme) of the Kabaret will be revealed and participants will be set free to start their videos! Groups will have 48 hours until the final screening of the Kabaret, where we will see their results!</p>
 
 <h3>The Lab</h3>
-<p>A Kabaret wouldn't be a Kabaret without a proper lab! The official MNKINO Lab will be located within the West Bank Social Center and will be open and monitored throughout the entirety of the 48 hours. Bring your equipment and set up shop directly following the initial screening!</p>
+<p>A Kabaret wouldn't be a Kabaret without a proper lab! The official MNKINO Lab will be located within the <a href="http://westbanksocialcenter.org" title="The WBSC">West Bank Social Center</a> and will be open and monitored throughout the entirety of the 48 hours. Bring your equipment and set up shop directly following the initial screening!</p>
 
 <h3>No Group? No Problem.</h3>
 <p>Individuals that don't arrive with a set group will be matched up with a group after the screening.</p>
 
 <h3>No Experience? No Problem!</h3>
-<p>Kabaret's are a great opportunity to learn the art of making short films! Just show up to the screening and we will match you up with a film-savvy group.</p>
+<p>Kabarets are a great opportunity to learn the art of making short films. Just show up to the screening and we will match you up with a film-savvy group.</p>
 
 <h2>What are the guidelines?</h2>
 
 <ol>
 	<li>Each group/individual must register each planned Kabaret film on the opening night of the MNKINO Kabaret. One person from each group will be designated as the primary director/contact.</li>
 	<li>Kabaret films must be made within the period of the Kabaret event. This includes all captured footage.</li>
- 	<li>Films must incorporate the Mystery Meat for the Kabaret. This will be announced on the opening night and posted on our website.</li>
+ 	<li>Films must incorporate the Mystery Meat (theme) for the Kabaret. This will be announced on the opening night and posted on our website.</li>
 	<li>Films must be <strong>no longer than 5 minutes long</strong>.</li>
 	<li>Completed film(s) must be submitted in digital file format (.mov, .avi, etc), Mini-DV format or on DVD. Double check that the file works before you submit!</li>
 	<li>All completed film(s) must be checked in by the designated primary director at the final screening. They msut be submitted by hand, 15 minutes before the beginning of the Kabaret screening.</li>
-	<li>Film must include the MNKINO KABARET logo and current year, in the end credits. (Our logo will be available for download).</li>
+	<li>Film must include the MNKINO logo and current year, in the end credits. (Our logo will be available for download).</li>
 	<li>Have fun and work as a team! Duh.</li>
 </ol>
 
-<p>Hope to see you at our FIRST EVER MNKINO KABARET! Remember, you can come with our without experience or technology. We will find you a team!</p>
+<p>Hope to see you at our first ever MNKINO Kabaret! Remember, you can come with our without experience or technology. We will find you a team!</p>
---
+++ pages/kabaret/signup.php
@@ -0,0 +1,6 @@
+<h1>Kabaret Signup</h1>
+<p>Enter all the important info here!</p>
+
+<div id="submit-form">
+<iframe src="http://spreadsheets.google.com/embeddedform?formkey=dFM1TXRRM2RVWWRNWEttV18wN0lvaEE6MQ" width="500" height="1048" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+</div>
