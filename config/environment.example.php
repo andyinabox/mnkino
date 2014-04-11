a6817f92feee524a11f7ed693e8a0eb85b6454a9
Andy Dayton <amdayton@gmail.com>
2014-04-11 13:38:58 -0400

    Updated from server backup

---
+++ config/environment.example.php
@@ -0,0 +1,15 @@
+<?php
+$CONFIG = array(
+	'web_root' => '/path/to/site/',
+	'mail_to' => 'mnkino@email.com',
+	'flickr_api_key' => '',
+	'admin_username' => '',
+	'admin_password' => '',
+	'blip_tv_username' => '',
+	'blip_tv_password' => '',
+	'blip_tv_url' => '',
+	'flickr_username' => '',
+	'flickr_password' => '',
+	'flickr_url' => ''
+);
+?>
